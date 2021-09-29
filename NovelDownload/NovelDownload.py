c2a4fcd6a4ba721c15104dc57513ce9b99b365d1
Frank Luo <FrankLuo666@gmail.com>
2021-09-30 01:16:25 +0900

    调整readme.md文件的格式。

--- NovelDownload/NovelDownload.py
+++ NovelDownload/NovelDownload.py
@@ -4,8 +4,6 @@ from bs4 import BeautifulSoup
 from itertools import islice
 import pandas as pd
 from tqdm import tqdm
-from selenium import webdriver
-from time import sleep
 
 headers ={
  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'
